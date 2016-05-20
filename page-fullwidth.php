606cfd5eeb31b0642f07947ec8d5d8a79f5a5725
Marcos Ribeiro <mrdezigner@gmail.com>
2016-05-20 11:45:05 -0300

    Hierarquia de arquivos do WordPress
    Páginas
    1. nomedapagina.php – Seria um arquivo para uma página especifica personalizada
    2. page.php
    3. index.php

--- page-fullwidth.php
+++ page-fullwidth.php
@@ -14,27 +14,26 @@ get_header(); ?>
 
 	<div class="col-md-12">
 
-		<?php /* The loop */ ?>
+	    <?php /* The loop */ ?>
 	    <?php while ( have_posts() ): the_post(); ?>
 
 	    <article id="post-<?php the_ID(); ?>" <?php post_class('post_item_single post_type_post post_format_ itemscope format-standard tag-image tag-post-formats tag-standard '); ?>>
-			<div class="post_featured">
-				<?php the_post_thumbnail( 'medium', array( 'class' => 'img-responsive' ) ); ?>
-			</div>
+		<div class="post_featured">
+			<?php the_post_thumbnail( 'medium', array( 'class' => 'img-responsive' ) ); ?>
+		</div>
 
-			<div class="post_header entry-header">
-				<h2 class="post_title entry-title" itemprop="headline"><?php the_title()?></h2>
-				<div class="post_date">
-					<span class="date updated" itemprop="datePublished"><?php the_time('d M Y');?></span>
-				</div>
+		<div class="post_header entry-header">
+			<h2 class="post_title entry-title" itemprop="headline"><?php the_title()?></h2>
+			<div class="post_date">
+				<span class="date updated" itemprop="datePublished"><?php the_time('d M Y');?></span>
 			</div>
-			<div class="post_content entry-content" itemprop="articleBody">
-				<?php the_content(); ?>
-			</div>
+		</div>
+		<div class="post_content entry-content" itemprop="articleBody">
+			<?php the_content(); ?>
+		</div>
 
 	    </article>
 
-	    <?php // comment_template(); ?>
 	    <?php endwhile; ?>
 
 	</div>

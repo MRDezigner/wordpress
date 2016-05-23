426e396009377a773b1ccb0db395f47495315c7c
Marcos Ribeiro <mrdezigner@gmail.com>
2016-05-22 22:37:08 -0300

    Update content.php

--- template-parts/content.php
+++ template-parts/content.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * The template part for displaying content
+ * A parte do modelo para exibir conteúdo
  *
  * @package WordPress
- * @subpackage Jacinta_Cavalcante
- * @since Jacinta Cavalcante 1.0
+ * @subpackage WordPress
+ * @since WordPress 1.0
  */
 ?>
 
@@ -18,7 +18,7 @@
 				<a href="<?php the_Permalink()?>"><?php the_time('d M Y');?></a>
 			</span>
 			<div class="post_counters post_counters_blog">
-				<a href="http://main.blog-cv.axiomthemes.com/standard-post-with-featured-image/#comments" class="post_counters_item post_counters_comments trx_addons_icon-comment">
+				<a href="<?php the_Permalink()?>#comments" class="post_counters_item post_counters_comments trx_addons_icon-comment">
 					<span class="post_counters_number"><?php comments_popup_link('0','1 ','% ');?></span>
 					<span class="post_counters_label">Comentário(s)</span>
 				</a>
@@ -32,26 +32,26 @@
 						<span class="social_items inited">
 							<span class="social_item social_item_popup">
 								<a href="#" class="social_icons social_twitter" data-link="#" data-count="twitter">
-									<span class="trx_addons_icon-twitter"></span><i>twitter</i>
+									<span class="icon-twitter"></span><i>twitter</i>
 								</a>
 							</span>
 							<span class="social_item social_item_popup">
 								<a href="#" class="social_icons social_facebook" data-link="#" data-count="facebook">
-									<span class="trx_addons_icon-facebook"></span><i>facebook</i></a>
+									<span class="icon-facebook"></span><i>facebook</i></a>
 							</span>
 							<span class="social_item social_item_popup">
 								<a href="#" class="social_icons social_gplus" data-link="#" data-count="gplus">
-									<span class="trx_addons_icon-gplus"></span><i>gplus</i>
+									<span class="icon-gplus"></span><i>gplus</i>
 								</a>
 							</span>
 							<span class="social_item social_item_popup">
 								<a href="#" class="social_icons social_tumblr" data-link="#" data-count="tumblr">
-									<span class="trx_addons_icon-tumblr"></span><i>tumblr</i>
+									<span class="icon-tumblr"></span><i>tumblr</i>
 								</a>
 							</span>
 							<span class="social_item social_item_popup">
-								<a href="mailto:contato@jacintacavalcante.com.br" class="social_icons social_mail" data-link="mailto:contato@jacintacavalcante.com.br" data-count="mail">
-									<span class="trx_addons_icon-mail"></span><i>mail</i>
+								<a href="mailto:contato@seudominio.com.br" class="social_icons social_mail" data-link="mailto:contato@jacintacavalcante.com.br" data-count="mail">
+									<span class="icon-mail"></span><i>mail</i>
 								</a>
 							</span>
 						</span>
@@ -62,7 +62,7 @@
 	</div><!-- .post_header -->
 	
 	<div class="post_featured with_thumb">
-		<?php the_post_thumbnail( 'medium', array( 'class' => 'attachment-cindy_j-thumb-med size-cindy_j-thumb-med wp-post-image' ) ); ?>
+		<?php the_post_thumbnail( 'medium', array( 'class' => 'wp-post-image' ) ); ?>
 		<div class="mask"></div>
 		<div class="icons">
 			<a href="<?php the_Permalink()?>" aria-hidden="true" class="icon-link"></a>

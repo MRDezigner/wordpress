78e1bc8c200b4de64c3df4b64093472a966ca9f3
Marcos Ribeiro <mrdezigner@gmail.com>
2016-05-22 22:25:38 -0300

    Update content-none.php

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -1,31 +1,31 @@
 <?php
 /**
- * The template part for displaying a message that posts cannot be found
+ * A parte do modelo para exibir uma mensagem que as mensagens não podem ser encontrados
  *
  * @package WordPress
- * @subpackage Jacinta_Cavalcante
- * @since Jacinta Cavalcante 1.0
+ * @subpackage WordPress
+ * @since WordPress 1.0
  */
 ?>
 
 <section class="no-results not-found">
 	<header class="page-header">
-		<h1 class="page-title"><?php _e( 'Nothing Found', 'jacintacavalcante' ); ?></h1>
+		<h1 class="page-title"><?php _e( 'Nada encontrado', 'wordpress' ); ?></h1>
 	</header><!-- .page-header -->
 
 	<div class="page-content">
 		<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
 
-			<p><?php printf( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'jacintacavalcante' ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
+			<p><?php printf( __( 'Pronto para publicar o seu primeiro post? <a href="%1$s">Comece aqui</a>.', 'wordpress' ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
 
 		<?php elseif ( is_search() ) : ?>
 
-			<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'jacintacavalcante' ); ?></p>
+			<p><?php _e( 'Desculpe, mas nada combina com seus termos de pesquisa. Por favor, tente novamente com algumas palavras-chave diferentes.', 'wordpress' ); ?></p>
 			<?php get_search_form(); ?>
 
 		<?php else : ?>
 
-			<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'jacintacavalcante' ); ?></p>
+			<p><?php _e( 'Parece que não conseguimos encontrar o que você está procurando. Talvez a pesquisa possa ajudar.', 'wordpress' ); ?></p>
 			<?php get_search_form(); ?>
 
 		<?php endif; ?>

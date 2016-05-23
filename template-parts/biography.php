3ae4eed77196d02a54c667259351df959de7a10e
Marcos Ribeiro <mrdezigner@gmail.com>
2016-05-22 22:21:18 -0300

    Update biography.php

--- template-parts/biography.php
+++ template-parts/biography.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * The template part for displaying an Author biography
+ * A parte do modelo para exibir uma biografia de Autor
  *
  * @package WordPress
- * @subpackage Jacinta_Cavalcante
- * @since Jacinta Cavalcante 1.0
+ * @subpackage WordPress
+ * @since WordPress 1.0
  */
 ?>
 
@@ -12,25 +12,25 @@
 	<div class="author-avatar">
 		<?php
 		/**
-		 * Filter the Jacinta Cavalcante author bio avatar size.
+		 * Filtro do tamanho do avatar do autor e bio do tema Wordpress.
 		 *
-		 * @since Jacinta Cavalcante 1.0
+		 * @since Wordpress 1.0
 		 *
-		 * @param int $size The avatar height and width size in pixels.
+		 * @param int $size A altura e largura do avatar em pixels.
 		 */
-		$author_bio_avatar_size = apply_filters( 'jacintacavalcante_author_bio_avatar_size', 42 );
+		$author_bio_avatar_size = apply_filters( 'wordpress_author_bio_avatar_size', 42 );
 
 		echo get_avatar( get_the_author_meta( 'user_email' ), $author_bio_avatar_size );
 		?>
 	</div><!-- .author-avatar -->
 
 	<div class="author-description">
-		<h2 class="author-title"><span class="author-heading"><?php _e( 'Author:', 'jacintacavalcante' ); ?></span> <?php echo get_the_author(); ?></h2>
+		<h2 class="author-title"><span class="author-heading"><?php _e( 'Author:', 'wordpress' ); ?></span> <?php echo get_the_author(); ?></h2>
 
 		<p class="author-bio">
 			<?php the_author_meta( 'description' ); ?>
 			<a class="author-link" href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author">
-				<?php printf( __( 'View all posts by %s', 'jacintacavalcante' ), get_the_author() ); ?>
+				<?php printf( __( 'View all posts by %s', 'wordpress' ), get_the_author() ); ?>
 			</a>
 		</p><!-- .author-bio -->
 	</div><!-- .author-description -->

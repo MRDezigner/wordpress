b1062f04a9582c878a486bb55a26840a1c0b5a27
Marcos Ribeiro <mrdezigner@gmail.com>
2016-05-25 09:12:10 -0300

    Update header
    Comentários sobre o Menu
    'menu'                    => 'primary', 
    'theme_location'    => 'primary',                             // Isso vc determinou na sua function.php
    'depth'                   => 2,                                         // Caso seu menu tenha submenus, aqui vc define qtos serão.
    'container'             => 'div',                                     // Vc determina que tipo de 'envolucro' será feito no teu menu, geralmente usamos uma div.
    'container_class'   => 'navbar-collapse collapse', // Aqui definimos a class ou class de nossa 'div' que foi determinada anteriormente
    'container_id'        => 'navigationbar',                   //  No toggle determinarmos uma data-target o id de lá tem que ser o mesmo aqui, se não o mobile não funciona
    'menu_class'        => 'nav navbar-nav',               // A class do menu propriamente dita
    'fallback_cb'         => 'wp_bootstrap_navwalker::fallback',
    'walker'                 => new wp_bootstrap_navwalker()) // Estamos declarando o arquivo de apoio, pois não uso o arquivo original do WordPress, ele deve estar na raiz do seu template

--- header.php
+++ header.php
@@ -15,10 +15,11 @@
 <html <?php language_attributes(); ?> class="no-js">
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
-
+	<link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/images/favicon.png">
+	
 	<?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<?php endif; ?>
@@ -28,6 +29,20 @@
 	<link rel='stylesheet' href='<?php bloginfo('template_directory'); ?>/css/font-awesome.css' type='text/css' media='all' />
 	<link rel='stylesheet' href='<?php bloginfo('template_directory'); ?>/css/fontello-embedded.min.css' type='text/css' media='all' />
 
+    	<!-- Add to homescreen for Chrome on Android -->
+    	<meta name="mobile-web-app-capable" content="yes">
+    	<link rel="icon" sizes="192x192" href="<?php bloginfo('template_directory'); ?>/images/touch/chrome-touch-icon-192x192.png">
+
+    	<!-- Add to homescreen for Safari on iOS -->
+    	<meta name="apple-mobile-web-app-capable" content="yes">
+    	<meta name="apple-mobile-web-app-status-bar-style" content="black">
+    	<meta name="apple-mobile-web-app-title" content="Material Design Lite">
+    	<link rel="apple-touch-icon-precomposed" href="<?php bloginfo('template_directory'); ?>/images/apple-touch-icon-precomposed.png">
+
+    	<!-- Tile icon for Win8 (144x144 + tile color) -->
+    	<meta name="msapplication-TileImage" content="<?php bloginfo('template_directory'); ?>/images/touch/ms-touch-icon-144x144-precomposed.png">
+    	<meta name="msapplication-TileColor" content="#3372DF">
+    
 	<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/html5.js"></script>
 
 	<?php wp_head(); ?>
@@ -40,7 +55,7 @@
 
 		<div class="col-md-12 text-center">
 			<a class="logo" href="<?php echo get_settings('home'); ?>">
-				<img src="<?php bloginfo('template_directory'); ?>/images/logo-big.png" class="img-responsive" alt="<?php the_title()?>" width="360" height="275">
+				<img src="<?php bloginfo('template_directory'); ?>/images/logo.png" class="img-responsive" alt="<?php the_title()?>" width="360" height="275">
 			</a>
 		</div>
 		
@@ -58,20 +73,19 @@
 			      </button>
 			    </div>
 			    
-		        <?php
-		            wp_nav_menu( array(
-		                'menu'              => 'primary',
-		                'theme_location'    => 'primary',
-		                'depth'             => 2,
-		                'container'         => 'div',
-		                'container_class'   => 'navbar-collapse collapse',
-		        		    'container_id'      => 'navigationbar',
-		                'menu_class'        => 'nav navbar-nav',
-		                'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
-		                'walker'            => new wp_bootstrap_navwalker())
-		            );
-		        ?>
+		            <?php
+		            	wp_nav_menu( array(
+		                	'menu'              => 'primary',
+		                	'theme_location'    => 'primary',
+		                	'depth'             => 2,
+		                	'container'         => 'div',
+		                	'container_class'   => 'navbar-collapse collapse',
+		        		'container_id'      => 'navigationbar',
+		                	'menu_class'        => 'nav navbar-nav',
+		                	'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+		                	'walker'            => new wp_bootstrap_navwalker())
+		            		);
+		            	?>
 			    </div>
 			</nav>
-
 		</header><!-- /header -->
